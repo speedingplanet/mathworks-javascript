8f2602511b464444451c72b9dda05cf793598182
John Paxton <pax@speedingplanet.com>
2021-07-14 16:16:02 -0400

    Promises!

---
+++ demos/async/promises.js
@@ -0,0 +1,23 @@
+const p1 = new Promise( ( resolve, reject ) => {
+  if ( Date.now() % 2 ) {
+    resolve( 'Success!' );
+  } else {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject( 'Failed!' );
+  }
+} );
+
+function onResolve( result ) {
+  console.log( 'The promise resolved successfully with', result );
+}
+
+function onReject( result ) {
+  console.error( 'The promise rejected (failed) with', result );
+}
+
+// p1.addEventListener('success', onResolve);
+// p1.addEventListener('error', onReject);
+// p1.then( onResolve );
+p1.then( onResolve, onReject ); // check the event queue
+
+console.log( 'After .then()' ); // check the event queue
