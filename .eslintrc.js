a871826a052f9030b7f2f9f8309a78377d3e4968
John Paxton <johnpaxton@gmail.com>
2022-09-13 21:46:00 -0400

    Change config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
       'error',
       { functions: 'never', arrays: 'always-multiline', objects: 'always-multiline' },
     ],
+    'no-unused-vars': 'warn',
     'prefer-const': 'off',
     semi: ['error', 'always'],
     'space-before-function-paren': ['error', { named: 'never' }],
