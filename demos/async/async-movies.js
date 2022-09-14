9d09a1e7959e2880b9ed6c3afbfc762a13a02445
John Paxton <johnpaxton@gmail.com>
2022-09-14 10:58:01 -0400

    Update demo

--- demos/async/async-movies.js
+++ demos/async/async-movies.js
@@ -1,1 +1,34 @@
 let moviesTable = document.querySelector('#movies-table tbody');
+
+async function fetchMovies() {
+  try {
+    // Think of 'await' as a block in this function
+    console.log('2: Fetching movies...');
+    let response = await fetch('http://localhost:8000/movies?_delay=2000');
+    if (response.ok) {
+      console.log('4: Fetched movies, parsing JSON results...');
+      let movies = await response.json();
+      console.log('5: Parsed results, building table...');
+      let tableRows = movies.map((movie) => {
+        let { title, year, rating, director } = movie;
+        let tr = document.createElement('tr');
+        tr.insertAdjacentHTML(
+          'beforeend',
+          `<td>${title}</td><td>${year}</td><td>${rating}</td><td>${director}</td>`
+        );
+        return tr;
+      });
+
+      console.log('6: Table complete, revealing...');
+      moviesTable.append(...tableRows);
+      moviesTable.parentElement.hidden = false;
+      console.log('7: Finished');
+    }
+  } catch (err) {
+    console.error('fetchMovies: Something went wrong:', err);
+  }
+}
+
+console.log('1: Before fetchMovies()');
+fetchMovies();
+console.log('3: After fetchMovies()');
